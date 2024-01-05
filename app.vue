5306c5f4b630e2d4a6b5e9fe435cd7ed437e9661
Aadith PM <aadithpm@gmail.com>
2024-01-05 00:18:44 -0600

    :mag: Add website info

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <Head>
+            <Title>Lowkey Photography</Title>
+            <Meta
+                name="lowkeyphotography"
+                content="Photography portfolio of Lokeshwar Deenadayalan"
+            />
+        </Head>
         <NuxtLayout>
             <Header></Header>
             <NuxtPage />
