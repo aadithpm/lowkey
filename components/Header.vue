34b9d0907f680839cf913c5cc592b4c5522025c7
Aadith PM <aadithpm@gmail.com>
2024-01-04 22:57:45 -0600

    :lipstick: Add header, more setup

---
+++ components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="header">
+        <div class="navbar left">
+            <ul>
+                <li>Photography</li>
+                <li>About</li>
+            </ul>
+        </div>
+        <div class="logo">
+            <img src="~assets/watermark.png" />
+        </div>
+        <div class="navbar right">
+            <ul>
+                <li>
+                    <span><img src="~assets/email.svg" /></span>
+                </li>
+                <li>
+                    <span><img src="~assets/instagram.svg" /></span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
